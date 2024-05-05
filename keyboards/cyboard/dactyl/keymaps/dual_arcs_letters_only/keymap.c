3854a8135639929fdc5770880b238ef0fc540d83
SilasMarvin <19626586+SilasMarvin@users.noreply.github.com>
2024-05-05 10:43:15 -0700

    Refactored config

--- keyboards/cyboard/dactyl/keymaps/dual_arcs_letters_only/keymap.c
+++ keyboards/cyboard/dactyl/keymaps/dual_arcs_letters_only/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [4] = LAYOUT_dual_arcs_let(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,      KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,  HOME_J,  HOME_K,  HOME_L,  HOME_SCLN, KC_QUOT,
-        _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RSFT,
-        _______, _______, _______, _______, MO(3), MO(1),   KC_ENT,          KC_SPC,  MO(2),   KC_BSPC, _______, _______, _______, _______,
-                                                     _______, _______,         _______, TO(0)
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RSFT,
+        _______, _______, _______, _______, KC_1,    KC_0,   KC_SPC,          KC_ENT,  MO(2),   KC_BSPC, _______, _______, _______, _______,
+                                                     _______, KC_LALT,       _______, TO(0)
     )
 };
