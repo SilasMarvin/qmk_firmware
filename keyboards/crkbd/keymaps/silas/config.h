59b8b6eff1b1e8011166696e975b07d9e4156145
Silas Marvin <19626586+SilasMarvin@users.noreply.github.com>
2025-07-29 19:46:54 -0700

    Some updates for mac

--- keyboards/crkbd/keymaps/silas/config.h
+++ keyboards/crkbd/keymaps/silas/config.h
@@ -43,19 +43,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
 
 // Left-hand home row mods
-#define HOME_A LGUI_T(KC_A)
+#define HOME_A LALT_T(KC_A)
 #define HOME_S LCTL_T(KC_S)
 #define HOME_D LSFT_T(KC_D)
-#define HOME_F LALT_T(KC_F)
+#define HOME_F LGUI_T(KC_F)
 
 // Right-hand home row mods
-#define HOME_J LALT_T(KC_J)
+#define HOME_J LGUI_T(KC_J)
 #define HOME_K RSFT_T(KC_K)
 #define HOME_L RCTL_T(KC_L)
-#define HOME_SCLN RGUI_T(KC_SCLN)
+#define HOME_SCLN RALT_T(KC_SCLN)
